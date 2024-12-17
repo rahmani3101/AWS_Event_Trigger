f0b255865808bb924846b56e9eec4dd357f9ee4f
Mohammad Asad Rahmani <146559979+rahmani3101@users.noreply.github.com>
2024-12-17 15:41:10 +0530

    Update s3-notification.sh

--- s3-notification.sh
+++ s3-notification.sh
@@ -10,7 +10,7 @@ echo "AWS Account ID: $aws_account_id"
 
 # Set AWS region and bucket name
 aws_region="us-east-1"
-bucket_name="abhishek-ultimate-bucket"
+bucket_name="falcon-ultimate-bucket"
 lambda_func_name="s3-lambda-function"
 role_name="s3-lambda-sns"
 email_address="zyz@gmail.com"
@@ -104,6 +104,6 @@ aws sns subscribe \
 aws sns publish \
   --topic-arn "$topic_arn" \
   --subject "A new object created in s3 bucket" \
-  --message "Hello from Abhishek.Veeramalla YouTube channel, Learn DevOps Zero to Hero for Free"
+  --message "Author : Mohammad Asad Rahmani"
 
 
